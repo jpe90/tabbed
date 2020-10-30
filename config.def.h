e3ee34376306665f6e7807208d60dc3c91a6419d
jpe90 <patriot.game@gmail.com>
2020-10-30 18:42:09 -0400

    changed font to gohu

--- config.def.h
+++ config.def.h
@@ -1,7 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char font[]        = "JetBrainsMono Nerd Font Mono:size=9";
+//static const char font[]        = "JetBrainsMono Nerd Font Mono:size=9";
+static const char font[]        = "GohuFont Nerd Font Mono:size=9";
 static const char* normbgcolor  = "#222222";
 static const char* normfgcolor  = "#cccccc";
 static const char* selbgcolor   = "#555555";
